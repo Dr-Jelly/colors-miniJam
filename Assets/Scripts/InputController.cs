7670001fd21039273b06ecc4c219fff49998b501
Dr-Jelly <44471313+Dr-Jelly@users.noreply.github.com>
2022-07-22 07:03:44 +0200

    InputController Singleton
    Made the InputController script into a singleton script (under Scripts/Utility).
    It now has one static Instance which gets overwritten on each new instantiation.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputController : MonoBehaviour
+public class InputController : Singleton<InputController>
 {
     [Header("References")]
     public PlayerInput Input;
