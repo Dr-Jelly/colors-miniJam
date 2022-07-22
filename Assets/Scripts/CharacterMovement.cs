6d777ebf10186e38a9d67dec8f18ed18da773f56
Dr-Jelly <44471313+Dr-Jelly@users.noreply.github.com>
2022-07-22 09:05:06 +0200

    Movement finetuning
    Added an acceleration parameter to smooth the characters movement.
    Also moved the CharacterMovement script into the script folder.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Rigidbody2D rb;
+
+    [Header("Movement Parameters")]
     [SerializeField] private float speed;
+    [Range(0.1f, 10)] [Tooltip("Velocity Changerate - Lower value makes the character movement smoother")]
+    [SerializeField] private float Acceleration;
 
     private void FixedUpdate()
     {
@@ -16,6 +21,7 @@ public class CharacterMovement : MonoBehaviour
     {
         direction.Normalize();
 
-        rb.velocity = direction * (speed * 100 * Time.fixedDeltaTime);
+        Vector2 vCurrent = direction * (speed * 100 * Time.fixedDeltaTime);
+        rb.velocity = Vector2.MoveTowards(rb.velocity, vCurrent, Acceleration);
     }
 }
